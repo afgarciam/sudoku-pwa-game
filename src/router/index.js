06323144902b2ac1b50879626b84e9707eaf3260
Alex Garcia <lxfrnnd1988@gmail.com>
2018-02-14 17:57:36 -0600

    Add dependencies firebase, axios and sudoku

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Board from '@/components/Board'
+import Home  from '@/components/Home'
+import About from '@/components/About'
+import Profile from '@/components/Profile'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
       path: '/board',
       name: 'Board',
       component: Board
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
     }
   ]
 })
