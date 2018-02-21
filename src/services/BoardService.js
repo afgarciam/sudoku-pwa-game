16b0b088ccbedb5c3c18c8a5db014e4e10150694
Alex Garcia <lxfrnnd1988@gmail.com>
2018-02-20 19:23:38 -0600

    Remove unused functions on BoardService.js

--- src/services/BoardService.js
+++ src/services/BoardService.js
@@ -32,9 +32,6 @@ export const BoardService = {
 
     window.localStorage.setItem('puzzle', JSON.stringify(puzzle))
 
-    // let board = this.boardFromSingleArray(object.puzzleGenerated)
-    // window.localStorage.setItem('board', JSON.stringify(board))
-
     return puzzle;
   },
 
@@ -42,55 +39,18 @@ export const BoardService = {
   savePuzzleRemote() {
     let puzzle = JSON.parse(window.localStorage.getItem('puzzle'))
     if (puzzle.id === null) {
-      // puzzle.puzzleBoard = this.boardToSingleArray(JSON.parse(window.localStorage.getItem('board')))
       firestore.collection(currentPuzzles).add(puzzle).then(docRef => {
         docRef.update({ id: docRef.id })
         puzzle.id = docRef.id
         window.localStorage.setItem('puzzle', JSON.stringify(puzzle))
       })
     } else {
-      // puzzle.puzzleBoard = this.boardToSingleArray(JSON.parse(window.localStorage.getItem('board')))
       puzzle.updated = new Date().toISOString()
       firestore.collection(currentPuzzles).doc(puzzle.id).update(puzzle).then(docRef => {
         window.localStorage.setItem('puzzle', JSON.stringify(puzzle))
       })
     }
 
-  },
-
-  //convert board (nested array) in sigle array to save on firestore
-  boardToSingleArray(puzzleBoard) {
-    let pos = 0;
-    let array = []
-    for (let i = 0; i <= 8; i++) {
-      for (let j = 0; j <= 8; j++) {
-        array[pos] = parseInt(puzzleBoard[i][j])-1
-        pos++;
-      }
-    }
-    return array
-  },
-
-  //convert single array to nested array to print in view
-  boardFromSingleArray(puzzle) {
-    let puzzleBoard = []
-    let puzzleRow = []
-    let countPos = 1;
-    let countRow = 0;
-    for (let i = 0; i < puzzle.length; i++) {
-      let number = puzzle[i] !== null ? parseInt(puzzle[i]) + 1 : null;
-      puzzleRow.push(number)
-      if (countPos === 9) {
-        puzzleBoard[countRow] = puzzleRow
-        puzzleRow = []
-        countPos = 1
-        countRow++
-      }
-      else {
-        countPos++
-      }
-    }
-    return puzzleBoard
   },
 
   humanBoard(el){
